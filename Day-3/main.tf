d998fb7d0b961feef9ad04c99b53ff9ec3fd887d
Mith29 <mithra29.venu@gmail.com>
2024-07-01 12:07:48 -0400

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-07d7e3e669718ab45" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value = "subnet-0599def636219b4bb". # replace this
 }
