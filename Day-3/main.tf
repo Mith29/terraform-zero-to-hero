58d6a6008dd036d6805471d2f858b733c8e41b02
Mith29 <mithra29.venu@gmail.com>
2024-07-01 12:10:02 -0400

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-07d7e3e669718ab45" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-0599def636219b4bb". # replace this
+  subnet_id_value = "subnet-0599def636219b4bb" # replace this
 }
